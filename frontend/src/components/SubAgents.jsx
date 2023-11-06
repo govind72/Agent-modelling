6ddeb38797c0c3c25b6e1e8409b9ac2722bda3e0
Govind Mali <mali.5@iitj.ac.in>
2023-11-06 15:10:59 +0530

    updated Subagents.js

--- frontend/src/components/SubAgents.jsx
+++ frontend/src/components/SubAgents.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const cardStyle = {
   marginBottom: '20px',
@@ -22,7 +23,7 @@ function SubAgents() {
     axios.get(`http://localhost:3000/agents/${id}`)
       .then(res => {
         const agent = res.data;
-        Promise.all(agent.subAgents.map(id => axios.get(`http://localhost:3000/agents/${id}`)))
+        Promise.all(agent.subAgents.map(subId => axios.get(`http://localhost:3000/agents/${subId}`)))
           .then(subAgentsData => {
             setSubAgents(subAgentsData.map(res => res.data));
           })
@@ -47,9 +48,36 @@ function SubAgents() {
                   {subAgent.name}
                 </Typography>
               </CardContent>
+              <CardContent>
+                <Button
+                  variant="contained"
+                  component={Link}
+                  to={`/agent/${subAgent.id}`}
+                >
+                  View Subagents
+                </Button>
+              </CardContent>
             </Card>
           </Grid>
         ))}
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Card variant="outlined" style={cardStyle}>
+            <CardContent>
+              <Typography variant="h6" component="div">
+                Add Agent
+              </Typography>
+            </CardContent>
+            <CardContent>
+              <Button
+                variant="contained"
+                component={Link}
+                to="/add-agent"
+              >
+                Add New Agent
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </div>
   );
